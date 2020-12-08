2eed61a262a27451494465ca581620e8daeff3fd
Devi Handevi <devihandevi@gmail.com>
2020-12-08 09:17:12 +0700

    [Parser] Add feature parsing from one pcap to RAW and MP3 files to a specific directory

--- audio_rtp_parser.py
+++ audio_rtp_parser.py
@@ -2,11 +2,17 @@ import pyshark
 import sox
 import os
 import sys
+import glob
 from datetime import datetime
+from operator import itemgetter
+from pathlib import Path
 
 RED_COLOR = '\033[91m'
 GREEN_COLOR = '\033[92m'
 NORMAL_COLOR = '\033[0m'
+
+LAYER_IP = 1
+LAYER_RTP = 3
 
 # Get arguments from cmd
 num_of_argv = len(sys.argv)
@@ -19,39 +25,47 @@ audio_folder_path = sys.argv[audio_folder_path_index] if audio_folder_path_index
 # If input and output params are not provided, return error
 if pcap_file_path == 0 or pcap_file_path[0] == '-':
   print(RED_COLOR + 'Input PCAP file path as parameter (-i /path/to/filename.pcap)' + NORMAL_COLOR)
-elif audio_folder_path == 0 or audio_folder_path[0] == '-':
+  raise
+if audio_folder_path == 0 or audio_folder_path[0] == '-':
   print(RED_COLOR + 'Input path to audio output folder path as parameter (-o /path/to/audio_output_folder/)' + NORMAL_COLOR)
-else:
-  # Reading PCAP
-  rtp_list = []
-  cap = pyshark.FileCapture(pcap_file_path, display_filter='rtp')
-  cap.set_debug()
+  raise
 
-  try:
-    for c in cap:
-      rtp = c[3]
-      if rtp.payload:
-        rtp_list.append(rtp.payload.split(":"))
-  except:
-    pass
-  finally: 
-    cap.close()
+# Reading PCAP
+rtp_list = []
+cap = pyshark.FileCapture(pcap_file_path, display_filter='rtp')
+cap.set_debug()
 
-  # Prepare temporaty files for the raw audio
-  timestamp = datetime.timestamp(datetime.now())
-  temp_filepath = '{}/{}.raw'.format(audio_folder_path, timestamp)
-  raw_audio = open(temp_filepath,'wb')
-  for rtp_packet in rtp_list:
-    packet = " ".join(rtp_packet)
-    audio = bytearray.fromhex(packet)
-    raw_audio.write(audio)
+i = 0
+filename = ''
+prev_src = ''
+prev_dst = ''
+try:
+  for c in cap:
+    cur_src = c[LAYER_IP].src
+    cur_dst = c[LAYER_IP].dst
+    # Prepare temporary files for the raw audio (either creating a new one or append to existing)
+    if cur_src != prev_src or cur_dst != prev_dst:
+      filename = cur_src + '-' + cur_dst
+      temp_filepath = '{}/{}.raw'.format(audio_folder_path, filename)
+      raw_audio = open(temp_filepath,'ab+')
+    # Get payload and write byte array to the file
+    rtp = c[LAYER_RTP]
+    if rtp.payload:
+      rtp_list = rtp.payload.split(":")
+      packet = " ".join(rtp_list)
+      audio = bytearray.fromhex(packet)
+      raw_audio.write(audio)
+except:
+  print(RED_COLOR + 'Error' + sys.exc_info()[0])
+  raise
+finally: 
+  cap.close()
 
-
-  # Read from raw and write audio files
+# Read from raw and write audio files
+raw_filepath_list = glob.glob("{}/*.raw".format(audio_folder_path))
+for raw_filepath in raw_filepath_list:
   tfm = sox.Transformer()
   tfm.set_input_format(rate=8000, channels=1, encoding='u-law')
-  status = tfm.build_file(temp_filepath, '{}/{}.mp3'.format(audio_folder_path, timestamp))
+  status = tfm.build_file(raw_filepath, '{}/{}.mp3'.format(audio_folder_path, Path(raw_filepath).stem))
 
-  print(GREEN_COLOR + 'Audio parsing finished!' + NORMAL_COLOR)
-
-  # python audio_rtp_parser.py -i D:/Downloads/coba_proses7.pcap -o D:/Downloads/output
+print(GREEN_COLOR + 'Audio parsing finished!' + NORMAL_COLOR)
