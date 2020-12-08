57d9dbbda6d3429895eba3e2e77ffd596b4eaf28
Devi Handevi <devihandevi@gmail.com>
2020-12-08 09:34:49 +0700

    Add checking file and folder existance and handle other errors

--- audio_rtp_parser.py
+++ audio_rtp_parser.py
@@ -7,10 +7,12 @@ from datetime import datetime
 from operator import itemgetter
 from pathlib import Path
 
+# Static variables for coloring CMD
 RED_COLOR = '\033[91m'
 GREEN_COLOR = '\033[92m'
 NORMAL_COLOR = '\033[0m'
 
+# Static variable to access layers in capture
 LAYER_IP = 1
 LAYER_RTP = 3
 
@@ -22,48 +24,63 @@ pcap_file_path_index = sys.argv.index(PCAP_FILE_PARAM) + 1 if PCAP_FILE_PARAM in
 audio_folder_path_index = sys.argv.index(AUDIO_FOLDER_PARAM) + 1 if AUDIO_FOLDER_PARAM in sys.argv else num_of_argv
 pcap_file_path = sys.argv[pcap_file_path_index] if pcap_file_path_index < num_of_argv else 0
 audio_folder_path = sys.argv[audio_folder_path_index] if audio_folder_path_index < num_of_argv else 0
+
 # If input and output params are not provided, return error
 if pcap_file_path == 0 or pcap_file_path[0] == '-':
   print(RED_COLOR + 'Input PCAP file path as parameter (-i /path/to/filename.pcap)' + NORMAL_COLOR)
-elif audio_folder_path == 0 or audio_folder_path[0] == '-':
+  exit()
+if audio_folder_path == 0 or audio_folder_path[0] == '-':
   print(RED_COLOR + 'Input path to audio output folder path as parameter (-o /path/to/audio_output_folder/)' + NORMAL_COLOR)
-else:
-  # Reading PCAP
+  exit()
+
+
+# Checking output folder existance
+if not os.path.exists(audio_folder_path):
+  os.makedirs(audio_folder_path)
+  print('Audio output folder created')
+
+# Reading PCAP
+try:
   rtp_list = []
   cap = pyshark.FileCapture(pcap_file_path, display_filter='rtp')
   cap.set_debug()
+except FileNotFoundError:
+  print(RED_COLOR + 'Input PCAP file {} does not exist'.format(pcap_file_path,) + NORMAL_COLOR)
+  exit()
+  
 
-  i = 0
-  filename = ''
-  prev_src = ''
-  prev_dst = ''
-  try:
-    for c in cap:
-      cur_src = c[LAYER_IP].src
-      cur_dst = c[LAYER_IP].dst
-      # Prepare temporary files for the raw audio (either creating a new one or append to existing)
-      if cur_src != prev_src or cur_dst != prev_dst:
-        filename = cur_src + '-' + cur_dst
-        temp_filepath = '{}/{}.raw'.format(audio_folder_path, filename)
-        raw_audio = open(temp_filepath,'ab+')
-      # Get payload and write byte array to the file
-      rtp = c[LAYER_RTP]
-      if rtp.payload:
-        rtp_list = rtp.payload.split(":")
-        packet = " ".join(rtp_list)
-        audio = bytearray.fromhex(packet)
-        raw_audio.write(audio)
-  except:
-    print(RED_COLOR + 'Error' + sys.exc_info()[0])
-    raise
-  finally: 
-    cap.close()
+# Init temporary variables
+i = 0
+filename = ''
+prev_src = ''
+prev_dst = ''
+try:
+  for c in cap:
+    cur_src = c[LAYER_IP].src
+    cur_dst = c[LAYER_IP].dst
+    # Prepare temporary files for the raw audio (either creating a new one or append to existing)
+    if cur_src != prev_src or cur_dst != prev_dst:
+      filename = cur_src + '-' + cur_dst
+      temp_filepath = '{}/{}.raw'.format(audio_folder_path, filename)
+      raw_audio = open(temp_filepath,'ab+')
+    # Get payload and write byte array to the file
+    rtp = c[LAYER_RTP]
+    if rtp.payload:
+      rtp_list = rtp.payload.split(":")
+      packet = " ".join(rtp_list)
+      audio = bytearray.fromhex(packet)
+      raw_audio.write(audio)
+except:
+  print(RED_COLOR + 'Error' + sys.exc_info()[0])
+  exit()
+finally: 
+  cap.close()
 
-  # Read from raw and write audio files
-  raw_filepath_list = glob.glob("{}/*.raw".format(audio_folder_path))
-  for raw_filepath in raw_filepath_list:
-    tfm = sox.Transformer()
-    tfm.set_input_format(rate=8000, channels=1, encoding='u-law')
-    status = tfm.build_file(raw_filepath, '{}/{}.mp3'.format(audio_folder_path, Path(raw_filepath).stem))
+# Read from raw and write audio files
+raw_filepath_list = glob.glob("{}/*.raw".format(audio_folder_path))
+for raw_filepath in raw_filepath_list:
+  tfm = sox.Transformer()
+  tfm.set_input_format(rate=8000, channels=1, encoding='u-law')
+  status = tfm.build_file(raw_filepath, '{}/{}.mp3'.format(audio_folder_path, Path(raw_filepath).stem))
 
-  print(GREEN_COLOR + 'Audio parsing finished!' + NORMAL_COLOR)
+print(GREEN_COLOR + 'Audio parsing finished!' + NORMAL_COLOR)
